a205c3385783b0dcbb734c078bf9311d4306384b
Luke Street <luke@street.dev>
2026-02-08 21:03:45 -0700

    Fix macOS build: export MAKEINFO=true
    MAKEINFO=true needs to be exported so sub-configures (bfd, etc.)
    inherit it, preventing makeinfo-not-found errors during parallel build.

--- build-macos.sh
+++ build-macos.sh
@@ -7,7 +7,8 @@ for patch in ../*.patch; do
   patch -N -p1 -i "$patch"
 done
 export CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.11"
-MAKEINFO=true ./configure --target=powerpc-xenon-pe --prefix="$PREFIX" --disable-nls --disable-shared --disable-gprof --disable-ld --disable-gold --without-zstd --with-system-zlib
+export MAKEINFO=true
+./configure --target=powerpc-xenon-pe --prefix="$PREFIX" --disable-nls --disable-shared --disable-gprof --disable-ld --disable-gold --without-zstd --with-system-zlib
 make -j$(nproc) configure-host
 make -j$(nproc)
 make install-strip
